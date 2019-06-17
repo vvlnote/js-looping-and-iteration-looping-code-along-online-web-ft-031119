43b40f9b83475d673cedbcca10c54684f507bc85
Joy Lin <vvlnote@hotmail.com>
2019-06-17 04:59:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+const nameArray = ["Lisa", "Keithlin", "Jan"];
+const giftType = "surprise"
+function writeCards(nameArray, giftType) {
+  const messageArr = [];
+  for (let i = 0; i < nameArray.length; i ++) {
+    messageArr[i] = `Thank you, ${nameArray[i]}, for the wonderful ${giftType} gift!`;
+  }
+  return messageArr;
+}
